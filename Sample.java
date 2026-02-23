09ff56a2494ce71f242cd32cf286e7947dabc521
Sayan Mandal <91383489+devSayan619@users.noreply.github.com>
2026-02-23 19:47:26 +0530

    Remove redundant print statement in Sample.java

--- Sample.java
+++ Sample.java
@@ -4,7 +4,6 @@ public class Sample
 	{
 		System.out.println("Hello from EC2 instance");
 		System.out.println("Hello");
-		System.out.print("hi");
 		System.out.println("Chanchi");
 		System.out.print("web hook Working");
 	}
